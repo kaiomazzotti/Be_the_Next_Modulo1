ae3e57d687c9cf90ddfe5d95ca1d329141aa66a1
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-12 18:00:41 -0300

    exercicio 1

--- ExerciciosVariados/ExerciciosVariados/Program.cs
+++ ExerciciosVariados/ExerciciosVariados/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Faça um programa que:
+//- Leia a cotação do dólar
+//- Leia um valor em dólares
+//- Converta esse valor para Real
+//- Mostre o resultado 
+
+Console.WriteLine("Qual a cotação do dolar? ");
+decimal cotacao = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Digite um valor em dólares: ");
+decimal dolar = decimal.Parse(Console.ReadLine());
+decimal real = cotacao * dolar;
+Console.WriteLine($"Convertendo para Real : {real.ToString("C")}");
+
+
+
