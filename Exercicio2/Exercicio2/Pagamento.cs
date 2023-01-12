4d4fc2c2d1bdb1fc5e3c4ed4d49f6a382b484e7f
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-12 18:43:10 -0300

    Exercicio 2

---
+++ Exercicio2/Exercicio2/Pagamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    public class Pagamento
+    {
+        public int IdVendedor { get;private set; }
+        private int CodigoPeca { get; set; }
+        private decimal Precopeca { get; set; }
+        private int QuantidadeVendida { get; set; }
+        public decimal Salario { get; private set; }
+        public decimal Comissao { get; set; }
+        private decimal PercentualComissao = 0.05M ;
+        public Pagamento(int idVendador)
+        {
+            IdVendedor = idVendador;
+            CodigoPeca = 0;
+            Precopeca = 0;
+            QuantidadeVendida = 0;
+            Salario = 0;
+        }
+        public void RegistrandoComissãoPorVenda()
+        {
+            string continua;
+            Console.WriteLine("Olá, seja bem vindo ao registro de comissão por venda!");
+            do
+            {
+                Console.WriteLine("Escaneie o código de barras ou digite o código da peça: ");
+                CodigoPeca = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite qual o preço da peça: ");
+                Precopeca = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a quantidade vendida: ");
+                QuantidadeVendida = int.Parse(Console.ReadLine());
+                Comissao = QuantidadeVendida * Precopeca * PercentualComissao;
+                Salario += Comissao;
+                Console.WriteLine($"Comissão de {Comissao.ToString("C")} adicionado com sucesso");
+                do
+                {
+                    Console.WriteLine("Deseja fazer mais algum registro? (S) para sim e (N) para não");
+                    continua = Console.ReadLine();
+                } while (continua.ToLower() != "s" && continua.ToLower() != "n");
+
+            } while (continua.ToLower() == "s");
+        }
+    }
+}
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Faça um programa para pagamento de comissão de vendedores de peças, levando-se em consideração que sua
+//comissão será de 5% do total da venda e que você tem os seguintes dados:
+//- Identificação do vendedor
+//- Código da peça
+//- Preço unitário da peça
+//- Quantidade vendida
+
+using Exercicio2;
+
+Pagamento vendedor = new Pagamento(1234);
+vendedor.RegistrandoComissãoPorVenda();
+Console.WriteLine($"O salário do vendedor ID({vendedor.IdVendedor}) fico em {vendedor.Salario.ToString("C")}");
