120bb55913f0faad4efe7253c3272c9a86164062
KaioMazzotti <kaio.mazzotti@gmail.com>
2023-01-13 10:00:56 -0300

    Exercício 4

---
+++ Exercicio4/Exercicio4/Conversao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    public static class Conversao
+    {
+        public static double ConverteGrauCpF(int celsius)
+        {
+            return Math.Round((9.0 * celsius + 160) / 5, 1);
+        }
+    }
+}
---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Ler uma temperatura em graus Celsius e apresentá-Ia convertida em graus Fahrenheit. A fórmula de conversão de
+//temperatura a ser utilizada é F = (9 * C + 160) / 5, em que a variável F representa é a temperatura em graus
+//Fahrenheit e a variável C representa é a temperatura em graus Celsius. 
+
+using Exercicio4;
+
+Console.WriteLine($"insira um temperatura em graus Cº para converter em Fº");
+int celsius = int.Parse(Console.ReadLine());
+Console.WriteLine($"{Conversao.ConverteGrauCpF(celsius)} Fº");
